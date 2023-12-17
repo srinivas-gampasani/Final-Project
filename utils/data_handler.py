a8bbe4e54b5ebb8581ae21381242fc75eb4fcbcc
srinivas-gampasani <149920911+srinivas-gampasani@users.noreply.github.com>
2023-12-17 22:08:50 +0530

    Update data_handler.py

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,12 +1,11 @@
-
 # utils/data_handler.py
 import json
 
-def read_data(file_path):
+def read_data_from_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-def write_data(file_path, data):
+def write_data_to_json(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
